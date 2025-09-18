de17e483071923ef8bdfb98f3569330ee01bbe91
Tremors <github@qtremors.anonaddy.me>
2025-09-18 14:22:37 +0530

    update

--- src/components/StarsBackground.tsx
+++ src/components/StarsBackground.tsx
@@ -120,7 +120,7 @@ const StarsBackground = () => {
           left: 0,
           width: '100%',
           height: '100%',
-          zIndex: -10,
+          zIndex: -8,
         }}
       />
     );
